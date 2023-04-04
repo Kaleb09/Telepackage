f998d4fb22beb57d3aaa03af3a1c36dd46abeabf
kaleb <blekaadamu979@gmail.com>
2023-04-04 03:34:53 -0700

    newcommit

--- app/src/main/java/com/kaleb/Telepackage/ClickListner.java
+++
@@ -1,9 +0,0 @@
-package com.kaleb.Telepackage;
-
-public interface ClickListner {
-    void toast();
-void data(String data);
-    void dialog(String step);
-    void foryou(String phone);
-}
-//commit
